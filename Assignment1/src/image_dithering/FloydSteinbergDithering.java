10dfd05aa001b45b3d1790d06aa2b45a4653705d
yf3 <yangfrancis0331@gmail.com>
2017-11-06 20:51:55 +0800

    Fix bug

--- Assignment1/src/image_dithering/FloydSteinbergDithering.java
+++ Assignment1/src/image_dithering/FloydSteinbergDithering.java
@@ -7,10 +7,10 @@ public class FloydSteinbergDithering extends ErrorDiffusionDithering {
     @Override
     void diffuseError(Mat output, double error, int currentRow, int currentCol) {
         MatPoint[] matPoints = new MatPoint[4];
-        matPoints[0] = new MatPoint(currentRow, currentCol + 1, 7);
-        matPoints[1] = new MatPoint(currentRow + 1, currentCol - 1, 7);
-        matPoints[2] = new MatPoint(currentRow + 1, currentCol, 7);
-        matPoints[3] = new MatPoint(currentRow + 1, currentCol - 1, 7);
+        matPoints[0] = new MatPoint(currentRow, currentCol + 1, 7.0);
+        matPoints[1] = new MatPoint(currentRow + 1, currentCol - 1, 3.0);
+        matPoints[2] = new MatPoint(currentRow + 1, currentCol, 5.0);
+        matPoints[3] = new MatPoint(currentRow + 1, currentCol - 1, 1.0);
         for (MatPoint p : matPoints) {
             if (!isOutOfBounds(output, p)) {
                 output.get(p.getRow(), p.getCol())[0] += (p.getWeight() / 16);
